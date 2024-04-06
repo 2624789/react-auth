428f0ce281eda9e20d4be915de5cc3b64d4284a9
2624789 <2624789@users.noreply.github.com>
2024-04-06 17:07:28 -0500

    Installed Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Typography from "@mui/material/Typography";
+
 const App = () => {
-  return <div>React Auth</div>;
+  return <div>
+    <Typography variant="h3">
+      React Auth
+    </Typography>
+  </div>;
 };
 
 export default App;
